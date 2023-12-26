bf7edf2da31b5aa98178f0be0649c2dd92d08a6c
Prithviraaj <z5419216@ad.unsw.edu.au>
2023-12-26 20:26:10 +1100

    init

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
-// import necessary resources to send requests to backend server
-const fetch = require('node-fetch');
+let activePage = 'home';
 
-function createOpenWindow() {
+const createOpenWindow = (pageName, linkedPage) => {
     // create a div element
     console.log("Hello World")
 
@@ -11,16 +10,52 @@ function createOpenWindow() {
     div.classList.add('open-window');
     parent.appendChild(div);
 
-    div.textContent = 'Hello World';
+    div.id = 'open-window-' + pageName;
+
+    let name = pageName;
+    name = name[0].toUpperCase() + name.slice(1);
+    name += ".html"
+
+    div.textContent = name;
+    focusOnWindow(pageName, linkedPage);
+}
+
+const focusOnWindow = (pageName, linkedPage) => {
+    document.getElementById(`open-window-${activePage}`).classList.remove('active-window');
+
+    const openWindow = document.getElementById(`open-window-${pageName}`);
+    openWindow.classList.add('active-window');
 }
 
 const openWindow = (pageName, linkedPage) => {
     // send get request to /pages and store json object
-    const json = null;
+    console.log("Opening");
+    const openPages = sessionStorage.getItem('openPages');
 
+    let openPagesJson = null;
+    if (openPages == null) {
+        // create new openPages object
+        openPagesJson = {
+            [`open-window-${pageName}`]: linkedPage
+        }
+
+        // store openPages object in session storage
+        sessionStorage.setItem('openPages', JSON.stringify(openPagesJson));
+    } else
+        openPagesJson = JSON.parse(openPages);
+
+    // check if page is already open
+    if (openPagesJson[`open-window-${pageName}`] == null) {
+        createOpenWindow(pageName, linkedPage);
+        openPagesJson[`open-window-${pageName}`] = linkedPage;
+    }
+    else {
+        // focus on window
+        const openWindow = document.getElementById(pageName);
+        focusOnWindow(openWindow);
+    }
+
+    sessionStorage.setItem('openPages', JSON.stringify(openPagesJson));
+    activePage = pageName;
 }
 
-const pageButtons = document.querySelectorAll('.page-button');
-pageButtons.forEach(button => {
-    button.addEventListener('click', openWindow);
-});
