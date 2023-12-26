49251ff90c627ba9aab0549a627640abe06bf323
Prithviraaj <z5419216@ad.unsw.edu.au>
2023-12-26 22:35:06 +1100

    upd tabbing

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
-let activePage = 'home';
+let activePage = '';
+
+const loadPage = (pageName, loadContent = false) => {
+    console.log("Switching to: " + pageName)
+    activePage = pageName;
+
+    if (!loadContent)
+        return;
+
+    content.innerHTML = "";
+    const object = document.createElement('object');
+    object.type = "text/html";
+    object.data = `./${pageName}.html`;
+    object.style.width = "100vh";
+    object.style.height = "100vh";
+    content.appendChild(object);
+}
 
 const createOpenWindow = (pageName, linkedPage) => {
     // create a div element
@@ -10,33 +26,60 @@ const createOpenWindow = (pageName, linkedPage) => {
     div.classList.add('open-window');
     parent.appendChild(div);
 
-    div.id = 'open-window-' + pageName;
+    div.id = `open-window-${pageName}`;
 
     let name = pageName;
     name = name[0].toUpperCase() + name.slice(1);
     name += ".html"
 
     div.textContent = name;
+    div.addEventListener('click', () => { focusOnWindow(pageName, linkedPage) });
     focusOnWindow(pageName, linkedPage);
 }
 
 const focusOnWindow = (pageName, linkedPage) => {
+    console.log("Focusing: " + pageName);
+
+    if (activePage === pageName)
+        return;
+
     document.getElementById(`open-window-${activePage}`).classList.remove('active-window');
 
     const openWindow = document.getElementById(`open-window-${pageName}`);
     openWindow.classList.add('active-window');
+
+    changeContent(linkedPage);
+    activePage = pageName;
+}
+
+const changeContent = (linkedPage) => {
+    const content = document.getElementById('content');
+    // content.innerHTML = `<object type="text/html" data="${linkedPage}" ></object>`;
+    // the above method causes a problem where the new data is loaded into a small section on the page with a scroll bar, it should take the full page
+    // the below method works
+    content.innerHTML = "";
+    const object = document.createElement('object');
+    object.type = "text/html";
+    object.data = linkedPage;
+    object.style.width = "100vh";
+    object.style.height = "100vh";
+    content.appendChild(object);
 }
 
 const openWindow = (pageName, linkedPage) => {
     // send get request to /pages and store json object
-    console.log("Opening");
+    console.log("Opening: " + pageName + ", " + activePage);
+
+    if (activePage === pageName)
+        return;
+
     const openPages = sessionStorage.getItem('openPages');
 
     let openPagesJson = null;
     if (openPages == null) {
         // create new openPages object
         openPagesJson = {
-            [`open-window-${pageName}`]: linkedPage
+            [`open-window-${activePage}`]: linkedPage
         }
 
         // store openPages object in session storage
@@ -44,18 +87,21 @@ const openWindow = (pageName, linkedPage) => {
     } else
         openPagesJson = JSON.parse(openPages);
 
+    console.log("JSON")
+    console.log(openPagesJson);
+
     // check if page is already open
     if (openPagesJson[`open-window-${pageName}`] == null) {
+        console.log("Opening New");
         createOpenWindow(pageName, linkedPage);
         openPagesJson[`open-window-${pageName}`] = linkedPage;
     }
     else {
         // focus on window
-        const openWindow = document.getElementById(pageName);
-        focusOnWindow(openWindow);
+        console.log("Opening Old");
+        focusOnWindow(pageName, linkedPage);
     }
 
     sessionStorage.setItem('openPages', JSON.stringify(openPagesJson));
-    activePage = pageName;
 }
 
