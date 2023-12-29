be1854e202ced1f2e48cf8749ae1938bd161a80b
Prithviraaj <z5419216@ad.unsw.edu.au>
2023-12-29 18:49:41 +1100

    fixed tab errors on refresh

--- js/index.js
+++ js/index.js
@@ -1,22 +1,63 @@
-let activePage = '';
+const loadOpenWindows = () => {
+    const openPages = sessionStorage.getItem('openPages');
+    // create the div elements for each open window
+
+    if (openPages == null)
+        return;
+
+    const openPagesJson = JSON.parse(openPages);
+
+    for (const key in openPagesJson) {
+        // check if tab already exists
+        const tab = document.getElementById(key);
+        if (tab)
+            continue;
+
+        const pageName = key.slice(12);
+        createOpenWindow(pageName, openPagesJson[key], false);
+    }
+
+    const activePage = sessionStorage.getItem('activePage');
+    console.log('Active: ' + activePage);
+    // focus on active page using activePage string
+    focusOnWindow(activePage, openPagesJson[`open-window-${activePage}`]);
+}
 
 const loadPage = (pageName, loadContent = false) => {
-    console.log("Switching to: " + pageName)
-    activePage = pageName;
+    console.log('LP Req: ' + pageName);
+
+    if (pageName === 'active') {
+        const activePage = sessionStorage.getItem('activePage');
+
+        console.log("LP Active: " + activePage);
+
+        if (activePage == null) {
+            loadPage('home', loadContent);
+            return;
+        }
+
+        loadPage(activePage, loadContent);
+        return;
+    }
+
+    console.log("LP Set Active to: " + pageName)
+    sessionStorage.setItem('activePage', pageName);
+
+    if (document.title === 'Portfolio')
+        loadOpenWindows();
+
+    const tab = document.getElementById(`open-window-${pageName}`);
+    if (tab && !tab.classList.contains('active-window'))
+        tab.classList.add('active-window');
+
 
     if (!loadContent)
         return;
 
-    content.innerHTML = "";
-    const object = document.createElement('object');
-    object.type = "text/html";
-    object.data = `./${pageName}.html`;
-    object.style.width = "100vh";
-    object.style.height = "100vh";
-    content.appendChild(object);
+    changeContent(`${pageName}.html`);
 }
 
-const createOpenWindow = (pageName, linkedPage) => {
+const createOpenWindow = (pageName, linkedPage, switchTab = true) => {
     // create a div element
     console.log("Hello World")
 
@@ -34,11 +75,14 @@ const createOpenWindow = (pageName, linkedPage) => {
 
     div.textContent = name;
     div.addEventListener('click', () => { focusOnWindow(pageName, linkedPage) });
-    focusOnWindow(pageName, linkedPage);
+
+    if (switchTab)
+        focusOnWindow(pageName, linkedPage);
 }
 
 const focusOnWindow = (pageName, linkedPage) => {
     console.log("Focusing: " + pageName);
+    const activePage = sessionStorage.getItem('activePage');
 
     if (activePage === pageName)
         return;
@@ -49,7 +93,8 @@ const focusOnWindow = (pageName, linkedPage) => {
     openWindow.classList.add('active-window');
 
     changeContent(linkedPage);
-    activePage = pageName;
+    sessionStorage.setItem('activePage', pageName);
+    console.log('FW Set Active Page: ' + pageName);
 }
 
 const changeContent = (linkedPage) => {
@@ -57,6 +102,8 @@ const changeContent = (linkedPage) => {
     // content.innerHTML = `<object type="text/html" data="${linkedPage}" ></object>`;
     // the above method causes a problem where the new data is loaded into a small section on the page with a scroll bar, it should take the full page
     // the below method works
+
+
     content.innerHTML = "";
     const object = document.createElement('object');
     object.type = "text/html";
@@ -68,12 +115,14 @@ const changeContent = (linkedPage) => {
 
 const openWindow = (pageName, linkedPage) => {
     // send get request to /pages and store json object
+    const activePage = sessionStorage.getItem('activePage');
     console.log("Opening: " + pageName + ", " + activePage);
 
     if (activePage === pageName)
         return;
 
     const openPages = sessionStorage.getItem('openPages');
+    loadOpenWindows();
 
     let openPagesJson = null;
     if (openPages == null) {
@@ -86,9 +135,6 @@ const openWindow = (pageName, linkedPage) => {
         sessionStorage.setItem('openPages', JSON.stringify(openPagesJson));
     } else
         openPagesJson = JSON.parse(openPages);
-
-    console.log("JSON")
-    console.log(openPagesJson);
 
     // check if page is already open
     if (openPagesJson[`open-window-${pageName}`] == null) {
